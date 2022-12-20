1baf55ae1c93958c2da1f177cb274f36e25b0ce2
Bouteveille jerome <formation@community-technologie.fr>
2022-12-20 08:04:08 +0100

    Body of method

--- src/test/java/fr/cm/paymybuddy/filterChainTest.java
+++ src/test/java/fr/cm/paymybuddy/filterChainTest.java
@@ -1,2 +1,15 @@
-package fr.cm.paymybuddy;public class filterChainTest {
+package fr.cm.paymybuddy;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class filterChainTest {
+
+
+
 }
